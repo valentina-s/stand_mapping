a6248b563836f2167fdd566d1b7a0e61db790cd9
David Diaz <ddiaz@ecotrust.org>
2020-04-27 15:06:51 -0700

    Adds NAIP fetching from The National Map

--- stand_mapping/utils/fetch_remote_data.py
+++ stand_mapping/utils/fetch_remote_data.py
@@ -45,7 +45,80 @@ def get_cover_from_extent(bbox, epsg, api_key,
                   },
               "weights": weights
               }
+    print('Retrieving image from AI for Earth Land Cover API', end='...')
     r = requests.post(BASE_URL, json=extent, headers=api_header).json()
     cover = imread(io.BytesIO(base64.b64decode(r['output_hard'])))
+    print('Done.')
 
     return cover
+
+
+def get_naip_tnm_url(xmin, ymin, xmax, ymax,
+                     bbox_epsg=3857,
+                     img_epsg=3857,
+                     img_format='jpgpng',
+                     width=500, height=500):
+  """
+  Returns the URL for making a request to get an image from The National Map's
+  NAIP REST service.
+
+  Parameters
+  ----------
+  minx, miny, maxx, maxy : numeric
+    the coordinates of the bounding box of the image to return, assumed to be
+    in coordinate reference system EPSG:3857
+  img_format : string
+    image format to request, valid options are 'jpg', 'jpgpng', 'png', 'png8',
+    'png24', 'png32', 'bmp', 'gif', 'tiff'.
+
+
+  Returns
+  -------
+  url : string
+    the url that will return the NAIP image
+  """
+
+  url = ''.join(['https://services.nationalmap.gov/arcgis/rest/services/',
+  'USGSNAIPImagery/ImageServer/exportImage?',
+  'bbox={}%2C{}%2C{}%2C{}&'.format(xmin, ymin, xmax, ymax),
+  'bboxSR={}&'.format(bbox_epsg),
+  'size={}%2C{}&'.format(width, height),
+  'imageSR={}&'.format(img_epsg),
+  'time=&',
+  'format={}&'.format(img_format),
+  'pixelType=U8&',
+  'noData=&',
+  'noDataInterpretation=esriNoDataMatchAny&',
+  'interpolation=+RSP_BilinearInterpolation&'
+  'compression=&',
+  'compressionQuality=&',
+  'bandIds=&',
+  'mosaicRule=&',
+  'renderingRule=&',
+  'f=image'])
+
+  return url
+
+
+def get_naip_from_tnm(bbox, **kwargs):
+  """
+  Retrieves a NAIP image from The National Map web service and returns it as
+  a numpy array.
+
+  Parameters
+  ----------
+  bbox : list-like
+    list of bounding box coordinates (minx, miny, maxx, maxy)
+
+  Returns
+  -------
+  img : array
+    NAIP image as a 3-band or 4-band array
+  """
+  url = get_naip_tnm_url(*bbox, **kwargs)
+  print('Retrieving NAIP image from The National Map', end='...')
+  r = requests.get(url)
+  img = imread(io.BytesIO(r.content))
+  print('Done.')
+
+  return img
