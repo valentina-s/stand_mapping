d9b0631c9c71f276fa9a90cc76e8ee27a9a584d9
David Diaz <ddiaz@ecotrust.org>
2020-05-04 14:18:45 -0700

    Adds seg2meancolor Fixes #5

--- stand_mapping/models/segmentation.py
+++ stand_mapping/models/segmentation.py
@@ -1,5 +1,8 @@
+import numpy as np
 from skimage.segmentation import (slic, quickshift, felzenszwalb, watershed,
                                   mark_boundaries)
+from skimage.color import label2rgb
+
 
 SEG_FUNCS = {
     'slic': slic,
@@ -53,4 +56,38 @@ def seg2labels(img, seg_func, with_boundaries=False, **kwargs):
         marked = mark_boundaries(img, labels, color=(1, 0, 0))
         return labels, marked
     else:
-        return segment_mask
+        return labels
+
+
+def seg2meancolor(img, seg_func, **kwargs):
+    """
+    Wrapper function that applies a segmentation routine to an image and
+    returns segmented image with the mean values of all pixels in a segment
+    assigned to all pixels in that segment.
+
+    Parameters
+    ----------
+    img : array
+      image to be segmented
+    seg_func : str or function
+      name of a segmentation function (currently supports 'slic', 'quickshift',
+      'felzenszwalb', and 'watershed') or a callable that accepts an image as
+      its first argument and returns an integer-labeled array.
+    **kwargs
+      keyword arguments that will be passed to the segmentation function.
+
+    Returns
+    -------
+    out : array
+      segmented image with meancolor assigned to segments
+    """
+    out = np.zeros_like(img)
+    label_img = seg2labels(img, seg_func, **kwargs)
+    labels = np.unique(label_img)
+
+    for label in labels:
+        mask = (label_img == label).nonzero()
+        color = img[mask].mean(axis=0)
+        out[mask] = color
+
+    return out
