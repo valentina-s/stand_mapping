542e282dcb4c23f28b5b34c3374feadfb90f933c
David Diaz <ddiaz@ecotrust.org>
2021-02-28 10:25:19 -0800

    Adding colorize_landcover function

--- stand_mapping/visualization/visualize.py
+++ stand_mapping/visualization/visualize.py
@@ -46,3 +46,28 @@ def draw_contours(dem,
         **kwargs)
 
     return cs
+
+
+def colorize_landcover(img, soft=False):
+    COLOR_MAP = {
+        0 : [1.0, 1.0, 1.0],  # unlabeled but mapped
+        1 : [0.0, 0.0, 1.0],  # water
+        2 : [0.0, 0.5, 0.0], # trees
+        3 : [0.5, 1.0, 0.5], # field
+        4 : [0.5, 0.375, 0.375],  # barren/non-vegetated\
+        5 : [0.0, 0.0, 0.0],  # developed/building
+        255 : [1.0, 0.0, 0.0]
+    }
+    cover_colors = np.zeros((img.shape[0], img.shape[1], 3))
+    if not soft:
+        for cov in np.unique(img):
+            mask = img == cov
+            cover_colors[mask] = COLOR_MAP[cov]
+    else: # we're given the probability of each class for each pixel
+        num_classes = img.shape[-1]
+        for i in range(num_classes):
+            cover_colors[:,:,0] += img[:,:,i] * COLOR_MAP[i+1][0] # R
+            cover_colors[:,:,1] += img[:,:,i] * COLOR_MAP[i+1][1] # G
+            cover_colors[:,:,2] += img[:,:,i] * COLOR_MAP[i+1][2] # B
+
+    return (cover_colors * 255).astype(np.uint8)
